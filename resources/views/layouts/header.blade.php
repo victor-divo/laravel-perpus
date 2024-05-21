08072d187a96f8f64acbb16232a6bf6fa695daa7
victor-divo-m <victordivo3133@gmail.com>
2024-05-21 16:43:19 +0700

    add bootstrap local

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,8 +7,7 @@
   <link rel="stylesheet" href="{{ asset('css/global.css') }}">
 
   {{-- Bootstrap 5 --}}
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet" crossorigin="anonymous">
   {{-- Bootstrap 4 --}}
   {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
     integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous"> --}}
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,8 +16,7 @@
     </div>
   </div>
   {{-- Bootsrap 5 --}}
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <script src="{{ asset('/js/bootstrap.bundle.min.js') }}" crossorigin="anonymous"></script>
 
   {{-- Bootstrap 4 --}}
   {{-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
