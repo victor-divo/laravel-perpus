e0b9336f046e0703f0d5fb17186b2c7dcc02386b
victor-divo-m <victordivo3133@gmail.com>
2024-06-07 01:52:08 +0700

    add local boostrap file

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,11 +4,13 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <!-- CSS Files -->
-  <link rel="stylesheet" href="{{ asset('css/global.css') }}">
+  {{-- <link rel="stylesheet" href="{{ asset('css/global.css') }}"> --}}
 
   {{-- Bootstrap 5 --}}
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> --}}
   {{-- Bootstrap 4 --}}
   {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
     integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous"> --}}
+
+  <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}/?v=1.1" />
